87827b8ce08e890f259a5144b906c6ee39b11fac
amjadgittuta <amjadgit@tutamail.com>
2024-08-25 08:47:33 +0530

    First Commit to the repository
    1. Created Default Application
    2. Added course-card component
    3. Modified course-card component to include course objects as parameter

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { CourseCardComponent } from './course-card/course-card.component';
+import { COURSES } from '../db-data';
+import { Course } from '../model/course';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, CourseCardComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'courseapp';
+
+  coreCourse = COURSES[0];
+  rxJSCourse = COURSES[1];
+  ngRXCourse = COURSES[2];
+
+}
